08ed412c500afcc8ef3638d1d79dc3659f840ac3
DariaIVA22 <myhusbandshrek@gmail.com>
2026-05-25 22:04:29 +0300

    "Добавлено сравнение ответов пользователя и правильных ответов. Подсчёт правильных ответов."

---
+++ lastpart.py
@@ -0,0 +1,12 @@
+def calculate_correct_answers(user_answers, correct_answers):
+    result = 0
+    for answer in range(len(correct_answers)):
+        if user_answers[answer] == correct_answers[answer]:
+            result+=1
+    return result
+
+a = ['a', 'd', 'c']
+b = ['a', 'b', 'd']
+
+print(calculate_correct_answers(a,b))
+
