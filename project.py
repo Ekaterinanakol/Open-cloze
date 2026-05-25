bb764f66d95fe595a2bb6ab5b977950dcb731e61
purpleheartx2 <myhusbandshrek@gmail.com>
2026-05-25 23:21:53 +0300

    Update project.py
    функцию сделала

--- project.py
+++ project.py
@@ -1,9 +1,8 @@
-answers = []
-for i in range(len(cleaned_tokens)):
-    if (i+1) == 10:
-        answers += i
-        cleaned_tokens[i] = "___"
-
-task = " ".join(cleaned_tokens)
-
-    
+def clean_tokens (formatted_text):
+    answers = []
+    for i in range(len(cleaned_tokens)):
+        if (i+1) == 10:
+            answers += i
+            cleaned_tokens[i] = "___"
+            task = " ".join(cleaned_tokens)
+    return task, answers
