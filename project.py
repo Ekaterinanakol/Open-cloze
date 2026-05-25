4277f42e179f59e3111982bb22193e0c840d3f33
mknazkova96 <mknazkova96@gmail.com>
2026-05-25 22:31:00 +0300

    1-2 пункты

---
+++ project.py
@@ -0,0 +1,11 @@
+import re
+import re
+
+def tokenize_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    words = re.findall(r'\b[a-zA-Z]+\b', text)
+    return words
+
+tokens = tokenize_file('humannature.txt')
+print(tokens)
